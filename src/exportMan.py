f5d18e2b7b245cfccf756455117b489fb0df4c44
roselone <roselonelh@gmail.com>
2018-01-08 22:19:10 +0800

    random mode

--- src/exportMan.py
+++ src/exportMan.py
@@ -11,28 +11,6 @@ import codecs
 from ScenEditor import *
 from functools import reduce
 
-class Hero(object):
-    def __init__(self, data):
-        self.hid     = int(data[0])
-        self.name    = data[1]
-        self.defense = int(data[2])
-        self.attack  = int(data[3])
-        self.IQ      = int(data[4])
-        self.EQ      = int(data[5])
-        self.charm   = int(data[6])
-        self.pikeman     = data[8]
-        self.halberdier  = data[9]
-        self.crossbowman = data[10]
-        self.cavalryman  = data[11]
-        self.weapon      = data[12]
-        self.navy        = data[13]
-        self.ability = data[14]
-        self.baseEvaluate = self.defense + self.attack + self.IQ
-    def __str__(self):
-        return str(self.mid) + ' ' + self.name + ' ' + self.ability
-    def __repr__(self):
-        return self.__str__()
-
 def readHero(fname):
     heroDic = {}
     f = codecs.open(fname, 'r', 'utf-8')
--- src/kingdom.py
+++ src/kingdom.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class Hero(object):
+    def __init__(self, data):
+        self.hid     = int(data[0])
+        self.name    = data[1]
+        self.defense = int(data[2])
+        self.attack  = int(data[3])
+        self.IQ      = int(data[4])
+        self.EQ      = int(data[5])
+        self.charm   = int(data[6])
+        self.pikeman     = data[8]
+        self.halberdier  = data[9]
+        self.crossbowman = data[10]
+        self.cavalryman  = data[11]
+        self.weapon      = data[12]
+        self.navy        = data[13]
+        self.ability = data[14]
+        self.baseEvaluate = self.defense + self.attack + self.IQ
+    def __str__(self):
+        return str(self.hid) + ' ' + self.name + ' ' + self.ability
+    def __repr__(self):
+        return self.__str__()
+
 class Kingdom(object):
     def __init__(self, king, capital, heroList):
         self.king = king
